1dbd63dd6aa74fb6211fd734a1af58b3c6415c57
TerribleTy <westty.05@gmail.com>
2016-10-11 18:09:25 -0400

    First commit

---
+++ src/main/java/com/terriblety/mod/FirstMod.java
@@ -0,0 +1,27 @@
+package com.terriblety.mod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class FirstMod {
+@EventHandler
+public void preInit(FMLPreInitializationEvent event)
+{
+	
+}
+@EventHandler
+public void init(FMLInitializationEvent event)
+{
+	
+}
+@EventHandler
+public void init(FMLPostInitializationEvent event)
+{
+	
+}
+}
+
---
+++ src/main/java/com/terriblety/mod/help/Reference.java
@@ -0,0 +1,7 @@
+package com.terriblety.mod.help;
+
+public class Reference {
+public static final String MODID = "terriblety_mod";
+public static final String NAME = "Mod";
+public static final String VERSION = "alpha-0.1";
+}
